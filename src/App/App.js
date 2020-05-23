362e0e448887e8b4663aaee79adc704d4881ac48
Joey Petrone <joey.petrone@gmail.com>
2020-05-22 22:43:18 -0500

    auth complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../helpers/data/connection';
+
+import Auth from '../components/Auth/Auth';
+import Navbar from '../components/Navbar/Navbar';
+import Home from '../components/Home/Home';
+
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <Home />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
+
     return (
       <div className="App">
-        <h2 className="m-3">React Hoarder App</h2>
-        <button className="btn btn-primary mt-3">Add All The Stuff</button>
+        <Navbar authed={authed}/>
+        <h2 className="m-3">React Hoarder</h2>
+        {loadComponent()}
       </div>
     );
   }
